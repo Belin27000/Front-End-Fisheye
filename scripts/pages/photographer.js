8478130bbd3dddf88987f889705d1f930ff6e288
Belin27000 <y.lecerf@protonmail.com>
2023-09-18 09:09:34 +0200

    feat: tech path mmodification for deploying on gh pages

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -11,7 +11,7 @@ async function init() {
 }
 
 async function getMedia() {
-    return fetch('./data/photographers.json')
+    return fetch(' ../../data/photographers.json')
         .then(function (data) {
             return data.json();
 
